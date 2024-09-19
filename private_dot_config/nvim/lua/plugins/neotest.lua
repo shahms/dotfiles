1138ccdf44d9ec69e994a787f759d53223a5fa97
Shahms King <shahms@shahms.com>
2024-09-19 15:14:45 -0700

    some neotest configuration

--- private_dot_config/nvim/lua/plugins/neotest.lua
+++ private_dot_config/nvim/lua/plugins/neotest.lua
@@ -1,9 +1,26 @@
 return {
   "nvim-neotest/neotest",
+  opts = function()
+    return {
+      adapters = {
+        require("neotest-python")({
+          python = "python3.11",
+        }),
+        require("neotest-plenary"),
+        require("neotest-vim-test")({
+          ignore_file_types = {"python", "vim", "lua"},
+        }),
+      },
+    }
+  end,
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter"
-  }
+    "nvim-treesitter/nvim-treesitter",
+    -- Adapters.
+    "nvim-neotest/neotest-plenary",
+    "nvim-neotest/neotest-python",
+    "nvim-neotest/neotest-vim-test",
+  },
 }
