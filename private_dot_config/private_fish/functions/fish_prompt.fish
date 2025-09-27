165633d305063d20f468fa11892f4e9430c265ba
Shahms E. King <shahms@shahms.com>
2025-09-26 17:57:40 -0700

    fix prompt coloring

--- private_dot_config/private_fish/functions/fish_prompt.fish
+++ private_dot_config/private_fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt
-  string join '' -- (set_color blue) "[$USER@" (prompt_hostname) ':' (prompt_pwd) "]"
-  string join '' -- (set_color red) '$ ' (set_color normal)
+    string join '' -- (set_color blue) "[$USER@" (prompt_hostname) ':' (set_color red) (prompt_pwd) (set_color blue) "]"
+    string join '' -- (set_color red) '$ ' (set_color normal)
 end
