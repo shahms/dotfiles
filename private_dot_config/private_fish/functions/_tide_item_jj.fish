afcfc7ed9e4d1b5648c6cfc20afeb0a7e6e08877
Shahms King <shahms@shahms.com>
2025-12-10 09:08:45 -0800

    silence jj snapshotting in fish

--- private_dot_config/private_fish/functions/_tide_item_jj.fish
+++ private_dot_config/private_fish/functions/_tide_item_jj.fish
@@ -2,16 +2,15 @@ function _tide_item_jj
     if not command -sq jj; or not jj root --quiet &>/dev/null
         return 1
     end
+    set -l log_args --no-graph --color=never --quiet --config ui.progress-indicator=false
 
-
-    set change_id (jj log --no-graph --color=never \
-        -r '@' -T 'change_id.shortest(8)')
+    set change_id (jj log $log_args -r '@' -T 'change_id.shortest(8)')
     string match -qr '(?<changes>#*)(?<bookmark>\w+)' \
-      (jj log --no-graph --color=never \
+        (jj log $log_args \
           -r 'closest_bookmark(@)::@' -T '"#" ++ self.bookmarks()')
     set ahead (math (string length $changes) - 1)
 
-    set jj_status (jj log -r@ -n1 --no-graph --color=never -T '
+    set jj_status (jj log $log_args -r@ -n1 -T '
     separate(" ",
         bookmarks.map(|x| if(
             x.name().substr(0, 10).starts_with(x.name()),
